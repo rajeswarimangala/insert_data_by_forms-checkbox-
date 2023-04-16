e74a3ccb8c99bac45d08f73f7d3706decbb96aab
rajeswarimangala <rajeswarimscm123@gmail.com>
2023-04-16 23:24:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from app.models import *
+def insert_topic(request):
+    if request.method=='POST':
+        tn=request.POST['tn']
+        TO=Topic.objects.get_or_create(topic_name=tn)[0]
+        TO.save()
+        return HttpResponse('Topic insertion is done Successfully')
+
+    return render(request,'insert_topic.html')
+
+def insert_webpage(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
+
+    if request.method=='POST':
+        topic=request.POST['topic']
+        name=request.POST.get('name')
+        url=request.POST.get('url')
+        email=request.POST['email']
+        TO=Topic.objects.get(topic_name=topic)
+
+        WO=Webpage.objects.get_or_create(topic_name=TO,name=name,url=url,email=email)[0]
+        WO.save()
+        return HttpResponse('Webpage insertion is done Successfully')
+
+
+    return render(request,'insert_webpage.html',d)
+
+def insert_access(request):
+    LTO=Webpage.objects.all()
+    d={'webpages':LTO}
+
+    if request.method=='POST':
+        name=request.POST['name']
+        author=request.POST['author']
+        date=request.POST['date']
+        WO=Webpage.objects.get(name=name)
+        AO=AccessRecord.objects.get_or_create(name=WO,author=author,date=date)[0]
+        AO.save()
+        return HttpResponse('Webpage insertion is done Successfully')
+
+    return render(request,'insert_access.html',d)
+
+def retrieve_data(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
+    if request.method=='POST':
+        td=request.POST.getlist('topic')
+        print(td)
+        webqueryset=Webpage.objects.none()
+
+        for i in td:
+            webqueryset=webqueryset|Webpage.objects.filter(topic_name=i)
+        d1={'webpages':webqueryset}
+        return render(request,'display_webpage.html',d1)
+    return render(request,'retrieve_data.html',d)
+
+def checkbox(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
+    return render(request,'checkbox.html',d)
+def radio_buttons(request):
+    LTO=Topic.objects.all()
+    d={"topics":LTO}
+    return render(request,'radio_buttons.html',d)
